68593249f2fdffad8da3b36071d905b5dc337f56
Owen O'Reilly <ooreilly@milan1.cm.cluster>
2026-07-09 11:19:52 -0400

    fix: clarity of comments in env variable setup of 000

--- 000.setup.sh
+++ 000.setup.sh
@@ -6,14 +6,16 @@
 module load amber/22
 module load chimera
 
-# not to bin
+# not to bin folder
 export DOCKHOME="/path/to/dock/installation/"
 
-# bin
+# not to bin folder# 
 export DMSHOME="/path/to/dms/installation"
 
+# path to bin folder
 export AMBERBIN="/path/to/amber/bin"
 
+# path to bin folder
 export CHIMERABIN="/path/to/chimera/bin"
 
 export ROOTDIR=`pwd`
